15b0154cef10a0adf37579a42176663b91ced74a
倪 李俊 <e.joshua.s.e@gmail.com>
2014-07-16 14:58:17 +0800

    07161457 BUG FIX
    修复了从Shop Tab进入Setting页并返回之后Shop Tab无法接受购物车消息的BUG。

--- Sources/Controllers/RootController.h
+++ Sources/Controllers/RootController.h
@@ -1,9 +1,22 @@
 
 #import "PivotController.h"
 #import "NewCustomerController.h"
+#import "LoginController.h"
+#import "SettingController.h"
+#import "LSShopViewController.h"
+#import "IntroduceController.h"
+#import "MoviePlayer.h"
+#import "MaterialController.h"
+#import "MessageController.h"
+
 
 //
 @interface RootController : PivotController
 {
 }
+
+@property LSShopViewController * shopVC;
+@property IntroduceController * intrVC;
+@property MaterialController * mateVC;
+@property MessageController * srVC;
 @end
